6843e4f518001dcbe63a261526c222f9a28054b2
Thrasys <tarcisio.word@gmail.com>
2020-06-30 17:11:47 -0300

    Primeiro commit / ex1 e ex2

---
+++ ex2/fibonacci/src/Fibo.java
@@ -0,0 +1,16 @@
+public class Fibo
+{
+    public static void main(String[] args)
+    {
+        int a = 1;
+        int b = 0;
+        int aux;
+
+        for (int i = 0; i < 30; i++) {
+            System.out.println(a);
+            aux = a;
+            a = a + b;
+            b = aux;
+        }
+    }
+}
